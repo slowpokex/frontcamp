72b3b46bf39bc11785b1dd73449fa4b42ca85180
Mikita Isakau <mikita_isakau@epam.com>
2017-12-12 16:44:23 +0300

    Fixed style loading && added dynamical import, but custom pugin haven't wrote yet

--- webpack.common.js
+++ webpack.common.js
@@ -2,33 +2,24 @@ const webpack = require('webpack');
 const devServer = require('webpack-dev-server');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 const path = require('path');
 
 // Common bundle
 module.exports = {
-  entry: path.join(__dirname, 'src/app/index.js'),
+  entry: {
+    main: path.join(__dirname, 'src/index.js')
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
+    chunkFilename: '[name].bundle.js',
+    filename: '[name].bundle.js',
     publicPath: '/',
   },
   resolve: {
     modules: ['node_modules'],
     extensions: ['.js', '.json', '.css', '.scss'],
   },
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin({
-      name: "vendor",
-      filename: "vendor.js",
-      minChunks: Infinity,
-    }),
-    new webpack.NoEmitOnErrorsPlugin(),
-    new webpack.optimize.OccurrenceOrderPlugin(),
-    new ExtractTextPlugin('bundle.css'),
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
-    })
-  ],
   module: {
     rules: [
       {
@@ -44,19 +35,34 @@ module.exports = {
       },
       {
         test: /\.s?css$/,
-        loader: ExtractTextPlugin.extract({
-          use: ['style-loader', 'css-loader', 'sass-loader'],
+        use: ExtractTextPlugin.extract({
+          use: ['css-loader', 'sass-loader'],
           fallback: 'style-loader'
         })
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: ['file-loader']
+        use: ['url-loader']
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ['file-loader']
+        use: ['url-loader']
       }
     ]
   },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: "vendor",
+      filename: "vendor.js",
+      minChunks: Infinity,
+    }),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new CleanWebpackPlugin(['dist']),
+    new ExtractTextPlugin('styles.css'),
+    new HtmlWebpackPlugin({
+      title: 'News API App',
+      template: './src/index.html'
+    })
+  ],
 };
